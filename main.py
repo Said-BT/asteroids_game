30231865132ca2b812db6260711245665539375f
Said-BT <s.boutsmoumt@gmail.com>
2025-01-25 12:56:37 +0100

    player, play rotation, player move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 black_color = (0, 0, 0)
 
@@ -9,16 +10,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
         screen.fill(black_color)
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
-    # print(f"Screen width: {constants.SCREEN_WIDTH}")
-    # print(f"Screen height: {constants.SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
